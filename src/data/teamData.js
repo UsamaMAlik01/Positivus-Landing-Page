61be0370036931d7ce84742ac3ab2285ea8725d3
Usama Malik <usamamalik60m@gmail.com>
2025-07-11 19:30:01 +0500

    images name updated

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -3,42 +3,42 @@ const teamData = [
         id: 1,
         name: "John Smith",
         title: "CEO and Founder",
-        image: "public/Team(1).png",
+        image: "public/Team1.png",
         description: "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy"
     },
     {
         id: 2,
         name: "Jane Doe",
         title: "Director of Operations",
-        image: "public/Team(2).png",
+        image: "public/Team2.png",
         description: "7+ years of experience in project management and team leadership. Strong organizational and communication skills"
     },
     {
         id: 3,
         name: "MIchael Brown",
         title: "Senior SEO Specialist",
-        image: "public/Team(3).png",
+        image: "public/Team3.png",
         description: "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization"
     },
     {
         id: 4,
         name: "Emily Johnson",
         title: "PPC Manager",
-        image: "public/Team(4).png",
+        image: "public/Team4.png",
         description: "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis"
     },
     {
         id: 5,
         name: "Brian Williams",
         title: "Social Media Specialist",
-        image: "public/Team(5).png",
+        image: "public/Team5.png",
         description: "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement"
     },
     {
         id: 6,
         name: "Sarah Kim",
         title: "Content Creator",
-        image: "public/Team(6).png",
+        image: "public/Team6.png",
         description: "2+ years of experience in content creation and management. Proficient in SEO-optimized writing and social media engagement"
     },
     {
