0a9de8434044f1e9a72e81980b0e23cc105c52fb
Usama Malik <usamamalik60m@gmail.com>
2025-07-11 19:42:31 +0500

    updated paths again

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -3,21 +3,21 @@ const teamData = [
         id: 1,
         name: "John Smith",
         title: "CEO and Founder",
-        image: "public/Team1.png",
+        image: "/Team1.png",
         description: "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy"
     },
     {
         id: 2,
         name: "Jane Doe",
         title: "Director of Operations",
-        image: "public/Team2.png",
+        image: "/Team2.png",
         description: "7+ years of experience in project management and team leadership. Strong organizational and communication skills"
     },
     {
         id: 3,
         name: "MIchael Brown",
         title: "Senior SEO Specialist",
-        image: "public/Team3.png",
+        image: "/Team3.png",
         description: "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization"
     },
     {
@@ -31,14 +31,14 @@ const teamData = [
         id: 5,
         name: "Brian Williams",
         title: "Social Media Specialist",
-        image: "public/Team5.png",
+        image: "/Team5.png",
         description: "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement"
     },
     {
         id: 6,
         name: "Sarah Kim",
         title: "Content Creator",
-        image: "public/Team6.png",
+        image: "/Team6.png",
         description: "2+ years of experience in content creation and management. Proficient in SEO-optimized writing and social media engagement"
     },
     {
